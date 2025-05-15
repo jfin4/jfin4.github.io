34e9e3018d3f0193a350d6fb54c303acafdebc11
John Inman <john@jfin.net>
2025-05-15 10:07:26 -0700

    no message

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ inject_template() {
 		    <meta name="viewport" content="width=device-width, initial-scale=1">
 		    <title>$2</title>
 		    <link rel="stylesheet" href="/assets/css/style.css">
-		    <link rel="icon" type="image/png" href="/assets/icon/favicon-96x96.png" sizes="96x96" />
-		    <link rel="icon" type="image/svg+xml" href="/assets/icon/favicon.svg" />
-		    <link rel="shortcut icon" href="/assets/icon/favicon.ico" />
-		    <link rel="apple-touch-icon" sizes="180x180" href="/assets/icon/apple-touch-icon.png" />
-		    <link rel="manifest" href="/assets/icon/site.webmanifest" />
+		    <link rel="icon" type="image/png" href="/assets/icons/favicon-96x96.png" sizes="96x96">
+		    <link rel="icon" type="image/svg+xml" href="/assets/icons/favicon.svg">
+		    <link rel="shortcut icon" href="/assets/icons/favicon.ico">
+		    <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-touch-icon.png">
+		    <link rel="manifest" href="/assets/icons/site.webmanifest">
 		  </head>
 		  <body>
 		    <header> 
