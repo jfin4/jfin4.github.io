fb17bfc1ed88a2b23d4c109fbbe5104968228494
John Inman <john@jfin.net>
2025-05-23 18:02:18 -0700

    no message

--- build.sh
+++ build.sh
@@ -1,37 +1,71 @@
 #!/bin/bash
 
-source_files=$(find content -name '*.md')
-for source in $source_files; do
-    dir=$(dirname $source)
-    target=$dir/public/index.html
+# process posts
+# ------------------------------------------------------------------------------
+
+temp=$(mktemp -d)
+entries=""
+
+for dir in posts/*; do
+    source=$dir/source.md
+    [[ -f $source ]] || continue
+    
+    # convert
+    target=$dir/_public/index.html
     if [[ $source -nt $target ]]; then
-        if [[ $target =~ '/about/' ]]; then
-            title=about
-            slug=about
-        else
-            title=$(head -n1 $source)
-            slug=$(head $source | grep -E '[0-9]{4}-[0-9]{2}-[0-9]{2}')
-        fi
-        content=$(pandoc $source)
-        export title content
-        [[ -d $dir/public ]] || mkdir $dir/public
+        export title=$(head -n1 $source)
+        export content=$(pandoc $source)
+        [[ -d $dir/_public ]] || mkdir $dir/_public
         envsubst < templates/index.html > $target
-        rsync -a --delete $dir/public/ content/public/$slug
     fi
-done 
 
-toc=""
-posts=$(find content/public | grep -E '[0-9]{4}-[0-9]{2}-[0-9]{2}$')
-for post in $posts; do
-    date=$(basename $post)
-    url=/$date
-    description=$(grep -Po '(?<=<title>).*?(?=</title>)' $post/index.html)
-    export date url description
-    toc_entry=$(envsubst < templates/toc.html)
-    toc="$toc"$'\n'$toc_entry
+    # copy to temp build dir
+    slug=$(head $source | grep -Eo "[0-9]{4}-[0-9]{2}-[0-9]{2}")
+    cp -r $dir/_public/* $temp/$slug
+
+    # make index entry
+    export date=$slug
+    export url=/$slug
+    export description=$(head -n1 $source)
+    entry=$(envsubst < templates/toc.html)
+    entries="$entries"$'\n'"$entry"
+
 done
-export title=jfin.net
-export content="$toc"
-envsubst < templates/index.html > content/public/index.html
 
-rsync -a --delete content/public/ public
+# make index
+export title=jfin.net
+export content="$entries"
+envsubst < templates/index.html > $temp/index.html
+
+# process pages
+# ------------------------------------------------------------------------------
+
+for dir in pages/*; do
+    source=$dir/source.md
+    [[ -f $source ]] || continue
+    
+    # convert
+    target=$dir/_public/index.html
+    if [[ $source -nt $target ]]; then
+        export title=$(basename $dir)
+        export content=$(pandoc $source)
+        [[ -d $dir/_public ]] || mkdir $dir/_public
+        envsubst < templates/index.html > $target
+    fi
+
+    # copy to temp build dir
+    slug=$(basename $dir)
+    cp -r $dir/_public/* $temp/$slug
+
+done
+
+# process root files
+# ------------------------------------------------------------------------------
+
+cp -r root/* $temp
+
+# move build dir to public
+# ------------------------------------------------------------------------------
+
+rm -r public
+cp -r $temp public
--- pages/about/source.md
+++ pages/about/source.md
@@ -2,6 +2,7 @@
 ![](johninman.jpg)
 :::
 
+
 Hi, I'm John. I work for the California State Water Resources Control Board
 where I assess the quality of the state\'s rivers, lakes, and streams.
  
